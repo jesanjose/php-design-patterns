d4d32b98642874f2451d040390d98662d6e9adeb
jesanjose <jesanjose@klab.com>
2014-03-25 13:51:05 +0800

    Added singleton pattern

---
+++ singleton/README.md
@@ -0,0 +1,7 @@
+Singleton
+===================
+* A pattern for an object that needs to be created once per request.
+
+#### When to use this pattern
+* You have an object that is used globally on the application.
+* This object needs to be instantiated once during the request.
---
+++ singleton/singleton.php
@@ -0,0 +1,32 @@
+<?php
+class Singleton
+{
+    /**
+     * Storage of the single instance
+     *
+     * @var $instance
+     */
+    protected static $instance;
+
+    /**
+     * Returns the stored instance in the static var
+     * If instance not found, it will create a new instance and store it in the static var.
+     *
+     * @return mixed
+     */
+    public static function getInstance()
+    {
+        if (static::$instance === null) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Private constructor to prevent call from outside.
+     */
+    private function __construct()
+    {
+    }
+}
